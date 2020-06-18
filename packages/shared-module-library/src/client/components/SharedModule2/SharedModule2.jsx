391dc34e3a9bbd40aaa03a86fea23d881e999804
kenta88 <a.dicrescenzo@elsevier.com>
2020-06-18 14:31:37 +0200

    add graphql

--- packages/shared-module-library/src/client/components/SharedModule2/SharedModule2.jsx
+++ packages/shared-module-library/src/client/components/SharedModule2/SharedModule2.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import axios from 'axios';
-import useAxios from 'axios-hooks';
+import { useQuery, ApolloProvider } from '@apollo/react-hooks';
+import { gql } from "apollo-boost";
 
+import ApolloClient from './ApolloClient';
 
 import './style.scss';
 
-const SharedModule2 = () => {
-  const [{ data, loading, error }, refetch] = useAxios(
-    'https://reqres.in/api/users?delay=1'
-  );
+const QUERY = gql`
+    {
+        characters(page: 1) {
+            results {
+                name
+            }
+        }
+    }
+`;
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error!</p>
+const SharedModule2 = () => {
+  const { loading, error, data } = useQuery(QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
   return (
     <div className="shared-module-2">
-      <button onClick={refetch}>refetch</button>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 };
 
-export default SharedModule2;
+const SharedModule2WithApollo = () => {
+  return (
+    <ApolloProvider client={ApolloClient}>
+      <SharedModule2 />
+    </ApolloProvider>
+  );
+};
+
+export default SharedModule2WithApollo;
